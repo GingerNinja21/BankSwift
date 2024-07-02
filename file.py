3c63280c06433ae209bc7fbfea9d5a0e16860186
GingerNinja21 <abubakr.adamson@gmail.com>
2024-07-02 13:40:30 +0200

    Password Generation (working)

--- file.py
+++ file.py
@@ -1,3 +1,25 @@
-###TESTING###
-def naah():
-    print()
+import math
+import random
+import bcrypt
+
+
+def password_generation():
+    password = ""
+    ascii_characters = [
+        '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>',
+        '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\',
+        ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        '{', '|', '}', '~'
+    ]
+    
+    while len(password) <= 11:
+        password += random.choice(ascii_characters)
+    
+
+    return password
+
+z= password_generation()
+print(z)
