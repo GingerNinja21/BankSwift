6dd96f57088d66e99748a1dcee9a563aebf1e403
GingerNinja21 <abubakr.adamson@gmail.com>
2024-07-07 14:58:11 +0200

    account_creation class ( password generation functionality)

--- file.py
+++ file.py
@@ -13,11 +13,10 @@ class DataValidation:
         self.Username = User_name.strip()
         self.Usersurname = User_surname.strip()
         self.id_no = id_no.strip() 
-        self.special_chars = ["-", "^"]  
+        self.special_chars = ["-", "^" ,"\'"]  
         self.error_message = ""  
 
         
-        # Validate inputs during initialization
         self.validate_username()
         self.validate_usersurname()
     
@@ -27,7 +26,6 @@ class DataValidation:
                 self.error_message += "\nPlease remove any special characters or numbers when entering your Name!"
                 break
 
-    
     def validate_usersurname(self):
         for char in self.Usersurname:
             if not (char.isalpha() or char in self.special_chars):
@@ -60,47 +58,34 @@ class DataValidation:
     def valid_acc_no(self):  
         global stored_account_no
         global account_bank
+
         try :
              with open("accounts.csv", "r") as file:
                 for line in file:
                     parts = line.strip().split(",")
-
+                    
                     if self.Username.lower() == parts[1] and self.Usersurname.lower() == parts[2]:
                         stored_account_no = parts[3].strip()
+                        self.error_message += "Account already exists!"
+                        return False
+                        break
 
-                        if len(stored_account_no) != 9:
+                    elif  len(stored_account_no) != 9:
                             
-                            print("if len works")
-                            self.error_message = "\nInvalid Account no! [too many char's]"
-                            break
+                        self.error_message += "\nInvalid Account no! [too many char's]"
                         return False    
+                        break
 
                     elif not stored_account_no.isdigit():
-                        self.error_message = "\nInvalid Account no! [Variable Type Error]"
+                        self.error_message += "\nInvalid Account no! [Variable Type Error]"
                         
                         break
 
-                    # else:
-                            # for line in file:
-                            #     parts = line.strip().split(",")
-                            #     print(stored_account_no,":",parts[3])
-                            #     if stored_account_no == parts[3] :
-                            #        self.error_message= "Account number is not unique!"
-                            #        return False
-
-                            #     else:
-                            #           if stored_account_no[:3] == "151" :
-                            #             account_bank = "Bankswift"
-                            #             print(account_bank)
-                            #             return True
-                         
-                            #           elif stored_account_no[:3] == "921" :
-                            #             account_bank = "FNB"
-                            #             return True
+                    else:
+                        return True
+                            
         except:
                 print("smth went wrong")
-
-
 
     def bank(self):
         global account_bank
@@ -121,19 +106,11 @@ class DataValidation:
                     
                     return account_bank
 
-
-
     def get_error_message(self):
+    
+       
         return self.error_message
                           
-
-    
-
-
-
-                                      
-        
-    
     def id_validation(self):
         try:
             if len(self.id_no) != 13 :
@@ -149,50 +126,94 @@ class DataValidation:
         except:
             self.error_message="Something went wrong! \nContact Administrator"
 
-def password_generation(name):
-    password = ""
-    ascii_characters = [
-        '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>',
-        '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\',
-        ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        '{', '|', '}', '~'
-    ]
+class account_creation:
+
+    def __init__(self, User_name, User_surname,id_no):
+        self.Username = User_name.strip().lower()
+        self.Usersurname = User_surname.strip().lower()
+        self.id_no = id_no.strip() 
+        self.special_chars = ["-", "^" ,"\'"]  
+
+        self.new_account = DataValidation(self.Username,self.Usersurname,self.id_no) 
+        
+       
+        self.new_account.id_validation()
+        self.new_account.account_existence()
+        self.new_account.valid_acc_no()
+        
+
+    def password_generation(self):
+        try:
+            print("account existence:",self.new_account.account_existence())
+            if self.new_account.account_existence() == True:
+               self.new_account.error_message += "\nAccount already exists!"
+
+               
+            
+            else:
+
+                password = ""
+                ascii_characters = [
+                    '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
+                    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>',
+                    '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\',
+                    ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                    '{', '|', '}', '~'
+                ]
+                
+
+
+                while len(password) <= 11:
+                    password += random.choice(ascii_characters)
+                    
+
+                    ### Storing Passwords ##
+                stored_passwords=[]
+                stored_passwords.append({"Name" : self.Username ,"Surname" : self.Usersurname ,"Password" : password})
+                    
+                file_name = "password_records.csv"
+                fields= ["Name" ,"Surname", "Password"]
+                    
+                with open(file_name , "a" , newline="") as csvfile:
+                    scribe = csv.DictWriter(csvfile , fieldnames = fields )
+                    ## Checks if file is empty ###
+                    if csvfile.tell () == 0:
+                        scribe.writeheader()
+                    
+                    scribe.writerows(stored_passwords)
+                    
+                  
+                return password
+        except:
+               print()
+               
+                    # return x
+
+    # def acc_no_generator(acc_type):
+    #     valid_char= ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
+    #     acc_type=""
+    #     gen_acc_no = ""
+
+    #     while len(gen_acc_no) != 9 :
     
 
 
-    while len(password) <= 11:
-        password += random.choice(ascii_characters)
-       
-
-    ### Storing Passwords ##
-    stored_passwords=[]
-    stored_passwords.append({"Name" : name , "Password" : password})
-
-    file_name = "password_records.csv"
-    fields= ["Name" , "Password"]
-
-    with open(file_name , "a" , newline="") as csvfile:
-        scribe = csv.DictWriter(csvfile , fieldnames = fields )
-        
-        ### Checks if file is empty ###
-        if csvfile.tell () == 0:
-            scribe.writeheader()
-
-        scribe.writerows(stored_passwords)
-
-    return password
 
 ### Testing area ###
-username="jOhn"
-usersurname= "doE"
+username="john"
+usersurname= "doe"
 id_no="0214536215243"
-x= DataValidation(username , usersurname, id_no)
-y= x.account_existence()
-r= x.valid_acc_no()
-z=x.get_error_message()
-t= x.bank()
-print("\nAccount existence:",y ,"\n error message:",z ,"\nValid account no:",r ,"\n", t )
+x= account_creation(username,usersurname,id_no)
+y= x.password_generation()
+z= x.new_account
+r= DataValidation(username,usersurname,id_no)
+c= r.get_error_message()
+print("c:",c)
+print*(error_message)
+# r= x.valid_acc_no()
+# z=x.get_error_message()
+# # t= x.bank()
+# print("\nAccount existence:",y ,"\n error message:",z ,"\nValid account no:",r ,"\n", t )
 
