258d5243fd57adc7ec631b2376e0e4d4efe15e29
simply.ands <agoodall021@student.wethinkcode.org.za>
2024-07-10 22:52:04 +0200

    email connection

--- main.py
+++ main.py
@@ -3,25 +3,24 @@ import requests
 import csv
 import random
 from io import StringIO
+from emails_utils import main as send_email
 
+stored_account_no = ""
+account_bank = "test"
 
-stored_account_no=""
-account_bank="test"
 ### Initialization ###
 class DataValidation:
 
-    def __init__(self, User_name, User_surname,id_no):
+    def __init__(self, User_name, User_surname, id_no):
         self.Username = User_name.strip()
         self.Usersurname = User_surname.strip()
         self.id_no = id_no.strip() 
-        self.special_chars = ["-", "^" ,"\'"]  
+        self.special_chars = ["-", "^", "\'"]  
         self.error_message = ""  
 
         self.validate_username()
         self.validate_usersurname()
         
-        # self.stored_account().account.creation
-    
     def validate_username(self):
         for char in self.Username:
             if not (char.isalpha() or char in self.special_chars):
@@ -37,7 +36,7 @@ class DataValidation:
     def account_existence(self):
         global stored_username
         global stored_usersurname
-        account_exists= False
+        account_exists = False
         try:
             with open("accounts.csv", "r") as file:
                 for line in file:
@@ -46,7 +45,7 @@ class DataValidation:
                     stored_username = parts[1].strip().lower()
                     stored_usersurname = parts[2].strip().lower()
                     if self.Username.lower() == stored_username and self.Usersurname.lower() == stored_usersurname:
-                        account_exists= True
+                        account_exists = True
                         break
                         
             if not account_exists:
@@ -54,19 +53,17 @@ class DataValidation:
         
             return account_exists
 
-           
-                    
         except FileNotFoundError:
             self.error_message += "\nError: Accounts file not found!"
-        except :
-            self.error_message+="Something went wrong! \nContact Administrator\n(Error location: account_existence)"
+        except:
+            self.error_message += "Something went wrong! \nContact Administrator\n(Error location: account_existence)"
 
     def valid_acc_no(self):  
         global stored_account_no
         global account_bank
 
-        try :
-             with open("accounts.csv", "r") as file:
+        try:
+            with open("accounts.csv", "r") as file:
                 for line in file:
                     parts = line.strip().split(",")
                     
@@ -74,91 +71,71 @@ class DataValidation:
                         stored_account_no = parts[3].strip()
                         self.error_message += "Account already exists!"
                         return False
-                        break
 
-                    elif  len(stored_account_no) != 9:
-                            self.error_message += "\nInvalid Account no! [too many char's]"
-                            return False    
-
+                    elif len(stored_account_no) != 9:
+                        self.error_message += "\nInvalid Account no! [too many char's]"
+                        return False
 
                     elif not stored_account_no.isdigit():
                         self.error_message += "\nInvalid Account no! [Variable Type Error]"
-                        
                         break
 
                     else:
                         return True
-                            
+
         except:
-                self.error_message+="Something went wrong! \nContact Administrator\n(Error location: valid_acc_no)"
+            self.error_message += "Something went wrong! \nContact Administrator\n(Error location: valid_acc_no)"
 
     def bank(self):
         global account_bank
         try:
             with open("accounts.csv", "r") as file:
-                    
-                    for line in file:
-                        parts = line.strip().split(",")
+                for line in file:
+                    parts = line.strip().split(",")
 
-                        if stored_account_no[:3] == "151" :
-                                account_bank = "Bankswift"
+                    if stored_account_no[:3] == "151":
+                        account_bank = "Bankswift"
 
-                        elif stored_account_no[:1] == "4" :
-                                if stored_account_no[:3] =="470" or stored_account_no[:4] =="4700":
-                                    account_bank = "Capitec"
-                                else:
-                                    account_bank = "ABSA"       
+                    elif stored_account_no[:1] == "4":
+                        if stored_account_no[:3] == "470" or stored_account_no[:4] == "4700":
+                            account_bank = "Capitec"
+                        else:
+                            account_bank = "ABSA"       
                                     
-                        elif stored_account_no[:1] == "6" :
-                                account_bank = "FNB"
+                    elif stored_account_no[:1] == "6":
+                        account_bank = "FNB"
                         
-                        elif stored_account_no[:1] == "1" or "0" :
-                                if stored_account_no[:2] == "15":
-                                    account_bank = "Nedbank"
-                                else: 
-                                    account_bank = "Standard Bank"
+                    elif stored_account_no[:1] == "1" or "0":
+                        if stored_account_no[:2] == "15":
+                            account_bank = "Nedbank"
+                        else: 
+                            account_bank = "Standard Bank"
             
-                        
-                        elif stored_account_no[:1] == "5" or "2" :
-                                account_bank = "Nedbank"
-                    return account_bank
+                    elif stored_account_no[:1] == "5" or "2":
+                        account_bank = "Nedbank"
+                    
+                return account_bank
+
         except:
-                self.error_message+="Something went wrong! \nContact Administrator\n(Error location: bank() function)"
+            self.error_message += "Something went wrong! \nContact Administrator\n(Error location: bank() function)"
 
     def get_error_message(self):
         return self.error_message
-                          
+
     def id_validation(self):
         try:
-            if len(self.id_no) != 13 :
+            if len(self.id_no) != 13:
                 self.error_message += "\nInvalid ID number!"
                 return False
             elif not self.id_no.isdigit():
                 self.error_message += "\nInvalid ID number! Remove any characters that are not Numbers!"
                 return False
             
-            else :
+            else:
                 return True
-        except:
-            self.error_message+="Something went wrong! \nContact Administrator\n(Error location: id_validation)"
 
-# new
-    # def password_recovery(self):
-    #     try:
-    #         with open("password_records.csv", "r") as csvfile:
-    #             reader = csv.DictReader(csvfile)
-    #             for row in reader:
-    #                 if (self.Username.lower() == row['name'].lower() and 
-    #                     self.Usersurname.lower() == row['surname'].lower() and
-    #                     self.id_no == row['id']):
-    #                     return f"Your password is: {row['password']}"
-                
-    #         return "Password not found. Please check your details and try again."
-        
-    #     except FileNotFoundError:
-    #         return "Password records file not found."
-    #     except Exception as e:
-    #         return f"Error retrieving password: {str(e)}"
+        except:
+            self.error_message += "Something went wrong! \nContact Administrator\n(Error location: id_validation)"
 
     def password_recovery(self):
         try:
@@ -179,18 +156,18 @@ class DataValidation:
             return "Password records file not found."
         except Exception as e:
             return f"Error retrieving password: {str(e)}"
-     
+
 class account_creation:
 
-    def __init__(self, User_name, User_surname,id_no,acc_type):
+    def __init__(self, User_name, User_surname, id_no, acc_type):
         self.Username = User_name.strip().lower()
         self.Usersurname = User_surname.strip().lower()
         self.id_no = id_no.strip() 
         self.acc_type = acc_type.strip().lower()
-        self.special_chars = ["-", "^" ,"\'"]  
+        self.special_chars = ["-", "^", "\'"]  
         self.error_message = ""  
 
-        self.new_account = DataValidation(self.Username,self.Usersurname,self.id_no) 
+        self.new_account = DataValidation(self.Username, self.Usersurname, self.id_no) 
         
         global valid_acc_no
         global valid_id
@@ -200,18 +177,18 @@ class account_creation:
         existing_account = self.new_account.account_existence()
         
     def get_error_message(self):
-        x= self.new_account.error_message
+        x = self.new_account.error_message
         return x
     
     def password_generation(self):
         try:
-            if existing_account == True:
-               self.new_account.error_message += "\nAccount already exists!" 
+            if existing_account:
+                self.new_account.error_message += "\nAccount already exists!" 
 
-            elif valid_id == False:
-                  self.new_account.error_message+=""
+            elif not valid_id:
+                self.new_account.error_message += ""
 
-            else:        
+            else:
                 password = ""
                 ascii_characters = [
                     '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
@@ -222,47 +199,39 @@ class account_creation:
                     'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                     '{', '|', '}', '~'
                 ]
-                
-
 
                 while len(password) <= 11:
                     password += random.choice(ascii_characters)
-                    
 
-                    ### Storing Passwords ##
-                stored_passwords=[]
-                stored_passwords.append({"Name" : self.Username ,"Surname" : self.Usersurname ,"Password" : password})
+                stored_passwords = []
+                stored_passwords.append({"Name": self.Username, "Surname": self.Usersurname, "Password": password})
                     
                 file_name = "password_records.csv"
-                fields= ["Name" ,"Surname", "Password"]
+                fields = ["Name", "Surname", "Password"]
                     
-                with open(file_name , "a" , newline="") as csvfile:
-                    scribe = csv.DictWriter(csvfile , fieldnames = fields )
-                    ## Checks if file is empty ###
-                    if csvfile.tell () == 0:
+                with open(file_name, "a", newline="") as csvfile:
+                    scribe = csv.DictWriter(csvfile, fieldnames=fields)
+                    if csvfile.tell() == 0:
                         scribe.writeheader()
                     
                     scribe.writerows(stored_passwords)
-                    
                 
                 return password
         except:
-                self.new_account.error_message += "\nSomthing went wrong! Contact Administrator!\n(Error location: password_generation)"  
-
-                    
+            self.new_account.error_message += "\nSomething went wrong! Contact Administrator!\n(Error location: password_generation)"  
 
     def acc_no_generator(self):
-        valid_char= ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
+        valid_char = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
         gen_acc_no = "151"
         valid_acc = False
         try:
-            while len(gen_acc_no) != 9 :
-                    gen_acc_no += random.choice(valid_char)
+            while len(gen_acc_no) != 9:
+                gen_acc_no += random.choice(valid_char)
 
             stored_account_no = gen_acc_no
             with open("accounts.csv", "r") as file:
-                while valid_acc == False:    
+                while not valid_acc:
                     for line in file:
                         parts = line.strip().split(",")
 
@@ -275,27 +244,27 @@ class account_creation:
                 return gen_acc_no
     
         except:
-             self.new_account.error_message += "\nSomething went wrong! Contact Administration!\n(Error location: acc_no_generator)"
+            self.new_account.error_message += "\nSomething went wrong! Contact Administration!\n(Error location: acc_no_generator)"
 
-    def store_account_from_gui(self, User_name, User_surname, id_no, acc_type):
-        try:
-            new_account_no = self.acc_no_generator()
-            stored_data = []
-            df = pd.read_csv("accounts.csv")
-            Uid = [df['uid'].max() + 1 if not df.empty else 0]
-            stored_data.append({"uid": Uid, "Name": User_name.strip().lower(), "Surname": User_surname.strip().lower(), "Account_no": new_account_no, "Balance": "0", "Account_type": acc_type.strip().lower(), "Id_no": id_no.strip(), "Linked_accounts": ""})
+    # def store_account_from_gui(self, User_name, User_surname, id_no, acc_type):
+    #     try:
+    #         new_account_no = self.acc_no_generator()
+    #         stored_data = []
+    #         df = pd.read_csv("accounts.csv")
+    #         Uid = [df['uid'].max() + 1 if not df.empty else 0]
+    #         stored_data.append({"uid": Uid, "Name": User_name.strip().lower(), "Surname": User_surname.strip().lower(), "Account_no": new_account_no, "Balance": "0", "Account_type": acc_type.strip().lower(), "Id_no": id_no.strip(), "Linked_accounts": ""})
 
-            file_name = "accounts.csv"
-            fields = ["uid", "Name", "Surname", "Account_no", "Balance", "Account_type", "Id_no", "Linked_accounts"]
+    #         file_name = "accounts.csv"
+    #         fields = ["uid", "Name", "Surname", "Account_no", "Balance", "Account_type", "Id_no", "Linked_accounts"]
 
-            with open(file_name, "a", newline="") as csvfile:
-                scribe = csv.DictWriter(csvfile, fieldnames=fields)
-                if csvfile.tell() == 0:
-                    scribe.writeheader()
-                scribe.writerows(stored_data)
-            return "Account created successfully!"
-        except Exception as e:
-            return f"Something went wrong! Contact Administrator!\n(Error location: store_account_from_gui)\n{str(e)}"
+    #         with open(file_name, "a", newline="") as csvfile:
+    #             scribe = csv.DictWriter(csvfile, fieldnames=fields)
+    #             if csvfile.tell() == 0:
+    #                 scribe.writeheader()
+    #             scribe.writerows(stored_data)
+    #         return "Account created successfully!"
+    #     except Exception as e:
+    #         return f"Something went wrong! Contact Administrator!\n(Error location: store_account_from_gui)\n{str(e)}"
 
 # Example usage
 username = "john"
@@ -307,7 +276,6 @@ ac = account_creation(username, usersurname, id_no, acc_type)
 result = ac.store_account_from_gui(username, usersurname, id_no, acc_type)
 print(result)
 
-
 ### Testing area ###
 # username="john"
 # usersurname= "doe"
@@ -317,7 +285,6 @@ print(result)
 
 # y= d.password_recovery()
 # print("Password Recovery Result:", y)
-
 
 # f= x.acc_no_generator()
 # c= x.get_error_message()
